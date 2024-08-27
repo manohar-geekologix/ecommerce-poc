c1a72f727355e59ff979fc8a3abc32546f5eb059
nikhilv12 <nikhilsinghns9900@gmail.com>
2024-08-27 11:55:58 +0530

    add

--- src/Utils/MockData.js
+++ src/Utils/MockData.js
@@ -5,4 +5,16 @@ export const users = [
         password: 'password123',
         phone: '1234567890',
     },
+    {
+        name:'Test one',
+        email:'test@mailinator.com',
+        password:'123456',
+        phone:9680905583,
+      },
+      {
+        name:'Test two',
+        email:'test2@mailinator.com',
+        password:'123456',
+        phone:9680905583,
+      }
 ];
