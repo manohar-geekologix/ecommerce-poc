315c0adfe1948cdc9e2be385825efe1baadc85b4
manohar-geekologix <manohar.geekologix@gmail.com>
2024-08-29 16:02:07 +0530

    logo update

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -47,7 +47,7 @@ const NavMenu = () => {
             <header className="text-[#213B85] text-primary-foreground py-3 lg:py-4 lg:px-5 border-b sticky top-0 bg-white">
                 <nav className="flex items-center justify-between px-5 lg:px-10">
                     <Link href="/" className="flex items-center gap-2 font-bold text-lg" prefetch={false}>
-                        <Image src={'/images/logo.svg'} width={100} height={100} className='size-full' />
+                        <Image src={'/images/header-logo.svg'} width={100} height={100} className='size-full' />
                     </Link>
                     <div className="hidden lg:block">{handleResponsive()}</div>
                     <div className="flex items-center gap-5 lg:text-xl">
